a686179db3a0d425208f3437e80b12b27b088e7a
akasharora963 <akasharora963@gmail.com>
2025-03-11 13:18:47 +0530

    initail: setup for stable coin

---
+++ src/StableCoin.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.13;
+/**
+ *@title StableCoin
+ *@author Akash Arora
+ *Collateral: Exogenous(wETH and wBTC)
+ *Minting: Algorthmic
+ *Relative Stability: Pegged to USD
+ *
+ *This contract is governed by Coin Engine.It is the implementaion of ERC-20 standard for stable coin system
+ */
+contract StableCoin {}
